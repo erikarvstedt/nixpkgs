b47d1473d044b4452cd38026b8826886e7103c00
Erik Arvstedt <erik.arvstedt@gmail.com>
2018-08-31 14:50:15 +0200

    fixup! django-flat-responsive: enable tests

--- pkgs/development/python-modules/django-flat-responsive/default.nix
+++ pkgs/development/python-modules/django-flat-responsive/default.nix
@@ -9,8 +9,6 @@ buildPythonPackage rec {
     sha256 = "0x3439m2bim8r0xldx99ry0fksfyv39k8bffnwpvahf500ksl725";
   };
 
-  doCheck = false;
-
   meta = with lib; {
     description = "An extension for Django admin that makes interface mobile friendly.";
     homepage = https://github.com/elky/django-flat-responsive;
