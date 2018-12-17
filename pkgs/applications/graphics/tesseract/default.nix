412a5b33213a9bc07498b43beca81a5ec01dc5e4
Erik Arvstedt <erik.arvstedt@gmail.com>
2018-12-17 08:42:59 +0100

    fixup! add earvstedt as a maintainer

--- pkgs/applications/graphics/tesseract/default.nix
+++ pkgs/applications/graphics/tesseract/default.nix
@@ -38,7 +38,7 @@ let
       description = "OCR engine";
       homepage = https://github.com/tesseract-ocr/tesseract;
       license = stdenv.lib.licenses.asl20;
-      maintainers = with stdenv.lib.maintainers; [viric];
+      maintainers = with stdenv.lib.maintainers; [ viric earvstedt ];
       platforms = with stdenv.lib.platforms; linux ++ darwin;
     };
   };
