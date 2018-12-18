79d7dbfe27d531517fd093201da4d66ceea27b69
Erik Arvstedt <erik.arvstedt@gmail.com>
2018-12-18 19:02:13 +0100

    tesseract: add a wrapper to setup languages
    Tesseract is now decoupled from the tessdata language corpus.
    
    This avoids recompilation when building Tesseract with a custom set
    of languages.
    
    Update k2pdfopt to use the new wrapper interface.

--- pkgs/applications/graphics/tesseract/default.nix
+++ pkgs/applications/graphics/tesseract/default.nix
@@ -3,9 +3,16 @@
 let
   tesseract3 = callPackage ./tesseract3.nix {};
   tesseract4 = callPackage ./tesseract4.nix {};
+  languages = callPackage ./languages.nix {};
 in
 {
-  tesseract = tesseract3;
+  tesseract = callPackage ./wrapper.nix {
+    tesseractBase = tesseract3;
+    languages = languages.v3;
+  };
 
-  tesseract_4 = lowPrio tesseract4;
+  tesseract_4 = lowPrio (callPackage ./wrapper.nix {
+    tesseractBase = tesseract4;
+    languages = languages.v4;
+  });
 }
