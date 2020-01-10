4648cef5b92662669754e3c0da8965f7a5005a2c
Erik Arvstedt <erik.arvstedt@gmail.com>
2020-01-10 22:44:49 +0100

    fix imgproxy

--- pkgs/servers/imgproxy/default.nix
+++ pkgs/servers/imgproxy/default.nix
@@ -1,8 +1,15 @@
 { lib, buildGoModule, fetchFromGitHub, pkg-config, vips, gobject-introspection }:
 
 buildGoModule rec {
-  version = "v2.8.1";
-  name = "imgproxy-${version}";
+  version = "2.8.1";
+  pname = "imgproxy";
+
+  src = fetchFromGitHub {
+    owner ="imgproxy";
+    repo = "imgproxy";
+    sha256 = "00hhgh6nrzg2blc6yl8rph5h5w7swlkbh0zgsj7xr0lkm10879pc";
+    rev = "v${version}";
+  };
 
   buildInputs = [
     gobject-introspection
@@ -10,24 +17,18 @@ buildGoModule rec {
     vips
   ];
 
-  src = fetchFromGitHub {
-    owner ="imgproxy";
-    repo = "imgproxy";
-    sha256 = "00hhgh6nrzg2blc6yl8rph5h5w7swlkbh0zgsj7xr0lkm10879pc";
-    rev = "${version}";
-  };
   preBuild = ''
     export CGO_LDFLAGS_ALLOW='-(s|w)'
   '';
 
   goPackagePath = "github.com/imgproxy/imgproxy";
 
-  modSha256 = "0w2k1v6zvk2kwh91idfisry3wly18ix8j13zjpl2q3hmv06mghpg";
+  modSha256 = "0kgd8lwcdns3skvd4bj4z85mq6hkk79mb0zzwky0wqxni8f73s6w";
 
   meta = with lib; {
     description = "Fast and secure on-the-fly image processing server written in Go";
+    homepage = https://imgproxy.net;
     license = licenses.mit;
     maintainers = with maintainers; [ paluh ];
-    homepage = https://imgproxy.net/;
   };
 }
