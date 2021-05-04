c9254f8a6559c30aba70d8cf260248d7d2e96a99
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-05-04 13:47:07 +0200

    btcpayserver/update.sh: auto-update nbxplorer
    It's now sufficient to run a single script (btcpayserver/update.sh) to
    update btcpayserver. This simplifies maintenance.
    nbxplorer is a strict dependency of btcpayserver.

--- pkgs/applications/blockchains/btcpayserver/update.sh
+++ pkgs/applications/blockchains/btcpayserver/update.sh
@@ -3,4 +3,7 @@ set -euo pipefail
 
 scriptDir=$(cd "${BASH_SOURCE[0]%/*}" && pwd)
 
+echo "Updating nbxplorer"
+"$scriptDir"/../nbxplorer/update.sh
+echo "Updating btcpayserver"
 "$scriptDir"/../nbxplorer/util/update-common.sh btcpayserver "$scriptDir"/deps.nix
