a8d47c1741c9ac6b080c2366860bad2137a5d9de
Flakebi <flakebi@t-online.de>
2021-05-13 16:41:57 +0200

    python3Packages.watchgod: init at 0.7

---
+++ pkgs/development/python-modules/watchgod/default.nix
@@ -0,0 +1,25 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+}:
+
+buildPythonPackage rec {
+  pname = "watchgod";
+  version = "0.7";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "SBQNYrDr6d2c+DgTN/BjUeHy5wsiA/qcbv9OVyyoTyk=";
+  };
+
+  # no tests in archive
+  doCheck = false;
+  pythonImportsCheck = [ pname ];
+
+  meta = with lib; {
+    description = "Simple, modern file watching and code reload in python.";
+    homepage = "https://github.com/samuelcolvin/watchgod";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ Flakebi ];
+  };
+}
