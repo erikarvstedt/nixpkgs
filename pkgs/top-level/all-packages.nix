316f40dcb2be2aba25b198aeaca49de8ce24eef7
Flakebi <flakebi@t-online.de>
2021-05-13 16:42:22 +0200

    paperless-ng: init at 1.4.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7471,6 +7471,8 @@ in
 
   paperless = callPackage ../applications/office/paperless { };
 
+  paperless-ng = callPackage ../applications/office/paperless-ng { };
+
   paperwork = callPackage ../applications/office/paperwork/paperwork-gtk.nix { };
 
   papertrail = callPackage ../tools/text/papertrail { };
