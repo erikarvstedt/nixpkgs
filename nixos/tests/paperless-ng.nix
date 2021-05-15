d599d3060af034028e9a1b8757338bc6062047ee
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-05-16 01:29:21 +0200

    fixup! test: break up long lines

--- nixos/tests/paperless-ng.nix
+++ nixos/tests/paperless-ng.nix
@@ -13,18 +13,24 @@ import ./make-test-python.nix ({ lib, ... }: {
   testScript = ''
     machine.wait_for_unit("paperless-ng-consumer.service")
 
-    # Create test doc
-    machine.succeed(
-        "convert -size 400x40 xc:white -font 'DejaVu-Sans' -pointsize 20 -fill black -annotate +5+20 'hello world 16-10-2005' /var/lib/paperless/consume/doc.png"
-    )
+    with subtest("Create test doc"):
+        machine.succeed(
+            "convert -size 400x40 xc:white -font 'DejaVu-Sans' -pointsize 20 -fill black "
+            "-annotate +5+20 'hello world 16-10-2005' /var/lib/paperless/consume/doc.png"
+        )
 
     with subtest("Service gets ready"):
         machine.wait_for_unit("paperless-ng-web.service")
         # Wait until server accepts connections
         machine.wait_until_succeeds("curl -fs localhost:28981")
 
-    # Create admin user
-    machine.succeed("echo \"from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@localhost', 'admin')\" | /var/lib/paperless/paperless-ng-manage shell")
+    with subtest("Create admin user"):
+        create_admin_cmd = (
+            "from django.contrib.auth import get_user_model;"
+            "User = get_user_model();"
+            "User.objects.create_superuser('admin', 'admin@localhost', 'admin')"
+        )
+        machine.succeed(f'echo "{create_admin_cmd}" | /var/lib/paperless/paperless-ng-manage shell')
 
     with subtest("Test document is consumed"):
         machine.wait_until_succeeds(
